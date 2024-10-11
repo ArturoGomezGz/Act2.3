ef20c64152bdd816d0768852a29d469bcfd556db
ArturoGomez <arturogomezgz04@gmail.com>
2024-10-11 15:55:54 -0600

    Comentarios en main.cpp

--- main.cpp
+++ main.cpp
@@ -4,14 +4,21 @@ using namespace std;
 
 int main()
 {
-    LinkedList list1 = LinkedList();
+    //Creacion de la lista1 vacia
+    LinkedList list1 = LinkedList(); 
+
+    // Carga del documento bitacora-1 a la lista1 este metodo crea la lista a pasrir del contenido del txt
     list1.addFile("bitacora-1");
 
+    // Ordena la lista1 usando el metodo de odenamiento merge sort
     list1.sortByIp();
+
+    // Exporta la lista1 (despues de ser ordenada) a un archivo bitacoraOrdenadaIP-Eq2.txt
     list1.exportToTxt("bitacoraOrdenadaIP-Eq2");
+
+    // Realiza n cantidad de busquedas y las exporta a un salidaN-Eq2.txt 
+    // Funcionamienot completo en "Linkedlist.h"
     list1.realizarBusqueda();
 
     return 0;
 }
-
-
